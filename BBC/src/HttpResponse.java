8a7c8c318648afd5b084429782d17f209b86b98d
ADY <devrimytr@yahoo.com>
2019-07-27 12:35:59 +0100

    Check validity of urls

--- BBC/src/HttpResponse.java
+++ BBC/src/HttpResponse.java
@@ -1,4 +1,24 @@
+import java.net.URL;
+import java.util.Scanner;
 
 public class HttpResponse {
-
+	private static boolean isValidURL(String theURL) {
+		try {
+			// Create a URL object from the String representation
+			URL requestURL = new URL(theURL);
+			// Use java.net.url class to validate the url
+			requestURL.toURI();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	String theURL = null;
+	while (scanner.hasNextLine()) {
+		theURL = scanner.nextLine();
+		isValidURL(theURL);
+	}
+}
 }
